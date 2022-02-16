7bf8ffbf1b9d81481c025b2664e0594197753862
KarthikS737 <lit2021012@iiitl.ac.in>
2022-02-17 00:59:32 +0530

    layer 1 done

---
+++ data.js
@@ -0,0 +1,61 @@
+[
+    {
+        "name": "child 1",
+        "children": [
+            {
+                "name": "A",
+                "children": [
+                    {
+                        "name": "X"
+                    },
+                    {
+                        "name": "Y"
+                    }
+                ]
+            },
+            {
+                "name": "B"
+            },
+            {
+                "name": "C"
+            },
+            {
+                "name": "D"
+            }
+        ]
+    },
+    {
+        "name": "child 2",
+        "children": [
+            {
+                "name": "E"
+            },
+            {
+                "name": "F"
+            },
+            {
+                "name": "G"
+            },
+            {
+                "name": "H"
+            }
+        ]
+    },
+    {
+        "name": "child 3",
+        "children": [
+            {
+                "name": "I"
+            },
+            {
+                "name": "J"
+            },
+            {
+                "name": "K"
+            },
+            {
+                "name": "L"
+            }
+        ]
+    }
+]
--- main.js
+++ main.js
@@ -1,20 +1,50 @@
 const tree = document.getElementById("tree");
+const app = document.getElementById("append");
+let node;
+let nodeElement;
 
+function iterobj(obj) {
+  for (val in obj) {
+    console.log("*" + node);
+    nodeElement = document.getElementById(node);
+    console.log(nodeElement);
 
-$(document).ready(
-    () => {
-        $.ajax({
-            type : 'GET',
-            url  : 'testData.js',
-            success : (res) => {
-                data = JSON.parse(res);
-                // console.log(tree);
-                for(val of data){
-                    if(val.hasOwnProperty('_id')){
-                        console.log(val);
-                    }
-                }
-            }
-        })
+    if (typeof obj[val] == "object") {
+      console.log(obj[val].name);
+      if (obj[val].hasOwnProperty("name")) {
+        nodeElement.innerHTML += `
+          <li> ${obj[val].name} 
+          <ul id="${obj[val].name.split(/\s+/).join("")}"></ul>
+          </li>
+        `;
+      }
+      iterobj(obj[val]);
+    } else {
+      // console.log(obj[val]);
+      // CODE PENDING
     }
-);
+  }
+}
+
+$(document).ready(() => {
+  $.ajax({
+    type: "GET",
+    url: "data.js",
+    success: (res) => {
+      data = JSON.parse(res);
+      //   console.log(res);
+      //   console.log(data);
+      // console.log(data[0].name)
+      for (parent of data) {
+        app.innerHTML += `
+          <li> ${parent.name} 
+           <ul id="${parent.name.split(/\s+/).join("")}"></ul>
+          </li>
+        `;
+        node = parent.name.split(/\s+/).join("");
+        iterobj(parent);
+        console.log("DONE");
+      }
+    },
+  });
+});
