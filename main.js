05621f6f6caf3983a57906f3802784585bcc464e
KarthikS737 <lit2021012@iiitl.ac.in>
2022-02-17 21:02:06 +0530

    modified:   index.html 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -1,62 +1,28 @@
 var path = "./assets/Images/";
-var ext1 = ".jpeg";
-var ext2 = ".jpg";
-var ext3 = ".png";
+let ext = [".jpeg", ".jpg", ".png", ".jfif"];
+
 $(document).ready(function () {
   $.ajax({
+    async: true,
     url: path,
     success: function (res) {
-      // console.log(res);
-      $(res)
-        .find("a:contains(" + ext1 + ")")
-        .each(function () {
-          var fileName = this.href
-            .replace(window.location.host, "")
-            .replace("http://", "");
-          $(".carousel-inner").append(
-            '<div class="carousel-item"><img class="d-block w-100" src=\'' +
-              fileName +
-              "' alt=\"Images here\"></div>"
-          );
-        });
+      for (var i = 0; i < ext.length; ++i) {
+        $(res)
+          .find("a:contains(" + ext[i] + ")")
+          .each(function () {
+            var fileName = this.href
+              .replace(this.host, "")
+              .replace("http://", "");
+            $(".carousel-inner").append(
+              '<div class="carousel-item"><img class="d-block w-100" src=\'' +
+                fileName +
+                '\' alt="Images here"></div>'
+            );
+          });
+      }
     },
   });
-  $.ajax({
-    url: path,
-    success: function (res) {
-      $(res)
-        .find("a:contains(" + ext2 + ")")
-        .each(function () {
-          var fileName = this.href
-            .replace(window.location.host, "")
-            .replace("http://", "");
-          $(".carousel-inner").append(
-            '<div class="carousel-item"><img class="d-block w-100" src=\'' +
-              fileName +
-              "' alt=\"Images here\"></div>"
-          );
-        });
-    },
+  $(".carousel").carousel({
+    interval: 250,
   });
-  $.ajax({
-    url: path,
-    success: function (res) {
-      $(res)
-        .find("a:contains(" + ext3 + ")")
-        .each(function () {
-          var fileName = this.href
-            .replace(window.location.host, "")
-            .replace("http://", "");
-          $(".carousel-inner").append(
-            '<div class="carousel-item"><img class="d-block w-100" src=\'' +
-              fileName +
-              "' alt=\"Images here\"></div>"
-          );
-        });
-    },
-  });
-  $('.carousel').carousel({
-    interval: 2000
-  })
 });
-
